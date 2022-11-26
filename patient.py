02fabd94fcb0413707a3f071472c93691c66c553
Arnold Stanovský <stanoarn@fit.cvut.cz>
2022-11-26 17:25:27 +0100

    add step parsing

--- patient.py
+++ patient.py
@@ -1,7 +1,12 @@
+import numpy as np
+import time
+import datetime
+
 class Patient():
 
-    def __init__(self, diagnosis=0, exercise=0, diet=0, medicaton=0) -> None:
+    def __init__(self, diagnosis, step_records=None, diet=None, medicaton=None) -> None:
         self._diagnosis = diagnosis
+        self._step_records = sorted(step_records, key=lambda item: item['date'])
 
     @property
     def diagnosis(self):
@@ -10,6 +15,22 @@ class Patient():
     @diagnosis.setter
     def diagnosis(self, diagnosis):
       self._diagnosis = diagnosis
+
+    @property
+    def step_records(self):
+      return self._step_records
+
+    def avg_steps_in_n_days(self, n):
+      now = time.mktime(time.localtime())
+      delta = datetime.timedelta(days=n).total_seconds()
+      potential_records = np.array([x['v'] for x in self._step_records if
+                                    (now - time.mktime(x['date'])) < delta])
+      total = 0
+      return np.mean(potential_records) #how good is the data??? are there days missing?
+
+    def total_avg_steps(self):
+      potential_records = np.array([x['v'] for x in self._step_records])
+      return np.mean(potential_records)
 
     @property
     def exercise(self):
