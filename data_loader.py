78535762e6d606a2a5908804a5491641f954c7c6
Arnold Stanovský <stanoarn@fit.cvut.cz>
2022-11-26 15:41:43 +0100

    refactor diag

--- data_loader.py
+++ data_loader.py
@@ -12,20 +12,39 @@ obvod pasu 👌
 
 """
 
+import json
+from os.path import join as path_join
+from patient import Patient
 
 class DataLoader:
-    def __init__(self) -> None:
+    def __init__(self, folder) -> None:
+        self.folder = folder
+
+    def MakePatient(self, folder) -> Patient:
+        dg_path = path_join(folder, 'dg.txt')
+        step_path = path_join(folder, 'steps.txt')
+        weight_path = path_join(folder, 'weight.txt')
+        waist_path = path_join(folder, 'waist.txt')
+
+        return Patient(
+             self.DgLoad(dg_path),
+             self.StepLoad(step_path),
+             self.WeightLoad(weight_path),
+             self.WaistLoad(waist_path)
+             )
+
+    def DgLoad(self, file):
+        diag = json.load(open(file, encoding='ISO-8859-2'))
+        diag = [x.split(':') for x in diag]
+        diag = [x if len(x) == 2 else [None, x[0]] for x in diag]
+        return diag
+
+
+    def StepLoad(self, file):
         pass
 
-
-    def DgLoad(self):
+    def WeightLoad(self, file):
         pass
 
-    def StepLoad(self):
-        pass
-
-    def WeightLoad(self):
-        pass
-
-    def WaistLoad(self):
+    def WaistLoad(self, file):
         pass
